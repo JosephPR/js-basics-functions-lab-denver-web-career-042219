36b83b2994b082ca6684d7654b8dfb8f4685c0ff
JosephPR <reilly4181@gmail.com>
2019-05-30 21:26:30 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(num){
+  let ts = 42
+    return Math.abs(num - ts)
+};
+
+function distanceFromHqInFeet(num1) {
+  return distanceFromHqInBlocks(num1) * 264
+};
+
+function distanceTravelledInFeet(num1, num2) {
+return Math.abs((num1 * 264)-(num2 * 264))
+};
